cf3ea3a837e9ad7e5e1bd61fb3b168c86e1d954f
Daniel Schmitz <dani.schmitz@live.de>
2016-10-28 09:58:38 +0200

    Added Login Component && added UserService functionality

---
+++ dev_public/app/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from "@angular/core";
+import {Router, RouterStateSnapshot, ActivatedRouteSnapshot, CanActivate} from "@angular/router";
+import {UserService} from "../../shared/user.service";
+import {Observable} from "rxjs/Observable";
+import {Observer} from "rxjs/Observer";
+
+@Injectable()
+
+export class AuthGuard implements CanActivate{
+
+
+    constructor(private userService: UserService, private router:Router) {
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):Observable<boolean> {
+        if (this.userService.isLoggedIn) { return Observable.of(true); }
+
+        return Observable.create((observer: Observer<any>) => {
+            this.userService.checkIfSessionExists().
+            subscribe(() => {
+                if(this.userService.isLoggedIn){
+                    observer.next(true);
+                    observer.complete();
+                }else{
+                    // Store the attempted URL for redirecting
+                    let redirectUrl = state.url;
+
+                    // Navigate to the login page
+                    this.router.navigate(['/login']);
+                    observer.next(false);
+                    observer.complete();
+                }
+            });
+        });
+    }
+}
